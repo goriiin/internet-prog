833c5c4450b07877e31bb787b25696d4ddfdeb14
dmitry <dkoshenkofff@mail.ru>
2023-10-20 13:40:17 +0300

    did task 4

--- train/task4/main.rb
+++ train/task4/main.rb
@@ -1,2 +1,24 @@
 # frozen_string_literal: true
 
+
+def char_lower?(char)
+  char.match?(/[a-z]/)
+end
+
+def char_upper?(char)
+  char.match?(/[A-Z]/)
+end
+
+def to_snake(str = '')
+  str.chars.map do |char|
+    if char_lower?(char)
+      char
+    elsif char_upper?(char)
+      "_#{char.downcase}"
+    end
+  end.join('')
+end
+
+str = gets.chomp
+
+puts to_snake(str)
