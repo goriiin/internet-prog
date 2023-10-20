62fa884c5e6e2fb981a1e5d5fc872ecc89461ee9
dmitry <dkoshenkofff@mail.ru>
2023-10-20 12:56:09 +0300

    did task 2

--- train/task2/main.rb
+++ train/task2/main.rb
@@ -1,2 +1,22 @@
 # frozen_string_literal: true
 
+require 'set'
+
+def sort_strings(arr)
+  arr.sort_by { |str| [-str.length, str] }
+end
+
+=begin
+arr = ['я', 'не', 'люблю', 'Бауманку', ', но’', 'я',
+       'люблю', 'семинары по ЯИП']
+=end
+
+size = gets.to_i
+arr = size.times.map { gets.chomp }
+
+my_set = Set[]
+
+arr.each { |item| my_set << item}
+
+
+p sort_strings my_set
