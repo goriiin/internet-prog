725cefe1c6d9128ab02261a047e5a50fa8221013
dmitry <dkoshenkofff@mail.ru>
2023-10-20 14:39:39 +0300

    did task 1

--- train/task1/main.rb
+++ train/task1/main.rb
@@ -1,32 +1,37 @@
 # frozen_string_literal: true
 
+def find_max_subarray(arr)
+  if arr.nil? || arr.empty?
+    puts 'empty array'
+    return
+  end
 
-def find(arr = [])
+  max_sum_start_index = 0
+  max_sum_last_index = 0
+  max_sum = arr[0]
 
+  last_sum_start_index = 0
+  last_sum = arr[0]
+
+  (1...arr.length).each do |i|
+    last_sum += arr[i]
+    if last_sum < arr[i]
+      last_sum = arr[i]
+      last_sum_start_index = i
+    end
+
+    next unless max_sum < last_sum
+
+    max_sum_start_index = last_sum_start_index
+    max_sum_last_index = i
+    max_sum = last_sum
+  end
+
+  puts "max_sum = #{max_sum}"
+  (max_sum_start_index..max_sum_last_index).each do |i|
+    print "#{arr[i]} "
+  end
 end
 
-def transform(arr)
-  sum = 0
-  arr.map { |num| sum += num }
-end
-
-def print(arr)
-  arr.each { |item| print "#{item} " }
-end
-
-size = gets.to_i
-
-arr = size.times.map { gets.to_i }
-
-p transform(arr)
-p find arr
-
-
-
-# arrays = Array.new(10) do
-#   length = rand(1..8)
-#   p Array.new(length) { rand(-10..10) }.sum
-# end
-
-
-
+arr = [1, -2, 3, 4, -9, 6]
+find_max_subarray(arr)
