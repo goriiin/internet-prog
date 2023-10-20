8fd41edf02416970c05a23a955d367348f2ec646
dmitry <dkoshenkofff@mail.ru>
2023-10-20 13:22:42 +0300

    did task 3

--- train/task3/main.rb
+++ train/task3/main.rb
@@ -1,2 +1,25 @@
 # frozen_string_literal: true
 
+def filter_range(arr, starting, ending)
+  arr.select { |item| item.between?(starting, ending) }
+end
+
+def calculate_arithmetic_mean(arr = [])
+  sum = 0
+
+  arr.each { |item| sum += item }
+  sum.to_f / arr.length
+end
+
+def answer(arr, starting, ending)
+  new_arr = filter_range arr, starting, ending
+  p new_arr
+
+  p calculate_arithmetic_mean(new_arr).round(2)
+end
+
+size = gets.to_i
+
+arr = size.times.map { gets.to_i }
+
+answer arr, 1, 5
